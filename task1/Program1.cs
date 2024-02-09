a5435127a5e85bf2f830ccba0ae79a6220fc2828
gevorghakobyan <gev.hakobyan93@gmail.com>
2024-02-09 11:02:41 +0400

    Lesson 4

---
+++ task1/Program1.cs
@@ -0,0 +1,39 @@
+﻿class Task1
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число или 'q' для выхода: ");
+        InputNumber();
+    }
+    static void InputNumber()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "q")
+            {
+                break;
+            }
+            int number;
+            if (int.TryParse(input, out number))
+            {
+                int sum = 0;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+
+                if (sum % 2 == 0)
+                {
+                    Console.WriteLine("[STOP]");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
+            }
+        }
+    }
+}
---
+++ task2/Program2.cs
@@ -0,0 +1,26 @@
+﻿class Task2
+{
+    static void Main(string[] args)
+    {
+        getEvenNumber(10);
+    }
+
+    static int getEvenNumber(int arraySize)
+    {
+        int[] numbers = new int[arraySize];
+        Random random = new Random();
+        int count = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000); //100 - 999
+            if (numbers[i] % 2 == 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine($"Массив {string.Join(",", numbers)}");
+        Console.WriteLine($"Количество чётных чисел в массиве. {count}");
+        return count;
+    }
+}
